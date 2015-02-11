4896343c4467f7988d117d720f6cf3b817d7e711
Steve Krouse <steveykrouse@gmail.com>
2015-02-11 13:57:00 -0500

    replaced underscore with lodash

--- react_components/editor/editorTop.js
+++ react_components/editor/editorTop.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var EditorArgument = require('./editorArgument');
-var _ = require('underscore');
+var _ = require('lodash');
 
 var EditorTop = React.createClass({
   displayName: 'EditorTop',
